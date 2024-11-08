662455f66bfcfe2260e17663617942d0abcaf6f2
Vikkavv <96725063+Vikkavv@users.noreply.github.com>
2024-11-08 13:58:25 +0100

    Add files via upload

--- Modelo/DAOProducto.php
+++ Modelo/DAOProducto.php
@@ -10,6 +10,18 @@ class DAOProducto{
         $this->conn = DB::getConnection();
     }
 
+    public function getProductByID($id){
+        $product = null;
+        $sql = "select * from Producto where id= ".$id;
+        $productInfo = $this->conn->query($sql);
+        if($productInfo->rowCount() > 0){
+            while ($row = $productInfo->fetch(PDO::FETCH_ASSOC)){
+                $product = new DTOProducto($row['id'], $row['nombre'],$row['descripcion'], $row['precio'], $row['cliente_id']);
+            }
+        }
+        return $product;
+    }
+
     public function getAllProducts(){
         $products = [];
         $sql = "select * from Producto";
@@ -21,4 +33,23 @@ class DAOProducto{
         }
         return $products;
     }
+
+
+    public function insertProduct($product){
+        $stmt = $this->conn->prepare("insert into Producto (nombre, descripcion, precio, cliente_id) values (:nombre, :descripcion, :precio, :cliente_id)");
+        $stmt->bindParam(":nombre", $product->getNombre());
+        $stmt->bindParam(":descripcion", $product->getDescripcion());
+        $stmt->bindParam(":precio", $product->getPrecio());
+        $stmt->bindParam(":cliente_id", $product->getClienteId());
+        $stmt->execute();
+    }    
+}
+
+$DAOProducto = new DAOProducto();
+
+if(isset($_POST['nombre'])){
+    $product = new DTOProducto($_POST['nombre'],$_POST['descripcion'],$_POST['precio'],$_POST['cliente_id']);
+    $DAOProducto->insertProduct($product);
+    header("Location: ../Vista/mainPage.php");
+    exit;
 }
