f806bacbbdcdc8722b4f1933c32afc5263748f91
Vikkavv <96725063+Vikkavv@users.noreply.github.com>
2024-11-12 23:34:22 +0100

    Add files via upload

--- Controlador/ControlProducto.php
+++ Controlador/ControlProducto.php
@@ -11,22 +11,35 @@ class ControlProducto{
 
     public function checkProductFields(DTOProducto $producto){
         $return = true;
-        $vName = "/[A-Za-z0-9]{5}/i";
-        if(preg_match($vName, $producto->getNombre())){
-            if($this->DAOProducto->getProductByID($producto->getId()) == $producto->getNombre()){
+        $vName = "/^[A-Za-z0-9]{5,}(( ){1}([A-Za-z0-9]*)?)*(.)?$/";
+        if($producto->getId() != null){
+            if(!$this->DAOProducto->getProductByID($producto->getId())){
+                $_SESSION['IdNotExists'] = true;
+                $return = false;
+            }
+        }
+        if(preg_match_all($vName, $producto->getNombre())){
+            if($this->DAOProducto->getProductByName($producto->getNombre()) != null && $producto->getId() == null){
                 $_SESSION['NameAE'] = true;
                 $return = false;
             }
         }
         else{
             $_SESSION['NameNotValid'] = true;
-            $return = false;
+            $return = false;   
         }
         if($producto->getPrecio() < 0){
-            $_SESSION['NegPrice'] = true;
-            $return = false;
+            if(is_numeric($producto->getPrecio())){
+                $_SESSION['NegPrice'] = true;
+                $return = false;
+            }
         }
-        $this->DAOProducto->insertProduct($producto);
+        if($return && $producto->getId() == null) $this->DAOProducto->insertProduct($producto);
+        elseif($return && $producto->getId() != null) $this->DAOProducto->updateProduct($producto);
         return $return;
-    }     
+    }
+
+    public function getDAOProducto(){
+        return $this->DAOProducto;
+    }
 }
--- Controlador/controlPeticionesProducto.php
+++ Controlador/controlPeticionesProducto.php
@@ -15,3 +15,31 @@ if(isset($_POST['insert'])){
     header("Location: $location");
     exit;
 }
+
+if(isset($_POST['update'])){
+    $realProduct = $controlProducto->getDAOProducto()->getProductByID($_POST['id']);
+    $product = new DTOProducto($_POST['nombre'], $_POST['descripcion'], $_POST['precio'], $_POST['cliente_id'], $_POST['id']);
+    
+    if($realProduct != false){
+        if(empty($_POST['nombre'])){
+            $product->setNombre($realProduct->getNombre());
+        }
+        if(empty($_POST['descripcion'])){
+            $product->setDescripcion($realProduct->getDescripcion());
+        }
+        if(empty($_POST['precio'])){
+            $product->setPrecio($realProduct->getPrecio());
+        }
+        if(empty($_POST['cliente_id'])){
+            $product->setClienteId($realProduct->getClienteId());
+        }
+    }
+    
+    $location = "";
+    if($controlProducto->checkProductFields($product)){
+        $location = "../Vista/mainPage.php";
+    }else $location = "../Vista/updateProduct.php";
+
+    header("Location: $location");
+    exit;
+}
