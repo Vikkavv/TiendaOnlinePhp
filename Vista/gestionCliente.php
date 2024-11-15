db28f94420b55d851a81a24591921e7458f54d04
Vikkavv <96725063+Vikkavv@users.noreply.github.com>
2024-11-15 11:57:55 +0100

    Add files via upload

--- Vista/gestionCliente.php
+++ Vista/gestionCliente.php
@@ -1,5 +1,7 @@
 <?php
+    require_once "../Modelo/DTOCliente.php";
     require_once "../Modelo/db.php";
+    app_security();
 ?>
 
 
@@ -8,16 +10,43 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Landing page</title>
+    <link rel="stylesheet" href="Assets/styles.css">
+    <title>Gestionar clientes</title>
 </head>
 <body>
-<h1>Menú</h1>
-<h3>¿Qué quieres hacer, <?php echo $_SESSION["nickname"]; ?>?</h3>
-
-<a href="editaCliente.php">Editar un cliente</a>
-<br>
-<a href="eliminaCliente.php">Eliminar un cliente</a>
-<br>
-<a href="mostrarClientes.php">Mostrar la base de clientes</a>
+    <nav>
+        <div>
+            <?php
+                $class = "hidden";
+                $nombre = "";
+                $apellido = "";
+                if(isset($_SESSION['cliente'])){
+                    $class = "";
+                    $nombre = $_SESSION['cliente']->getNombre();
+                    $apellido = $_SESSION['cliente']->getApellido();
+                }
+                print '<p class="'.$class.'">Administrador '.$nombre.' '.$apellido.'</p>'
+            ?>
+        </div>
+        <div>
+            <a href="index.php">Página Principal</a>
+            <a href="inicioSesion.php">Iniciar Sesión</a>
+            <a href="registro.php">Registrarse</a>
+            <a href="trastienda.php">Trastienda</a>
+            <a href="carrito.php"><img src="Assets/img/carrito.png" alt="icono carrito de la compra"></a>
+        </div>
+    </nav>
+    <h1 class="center xxl">Clientes</h1>
+    <div class="container grid-3">
+        <a href="editaCliente.php" class="card">
+            <p class="xxl">Editar un cliente</p>  
+        </a>
+        <a href="eliminaCliente.php" class="card">
+            <p class="xxl">Eliminar un cliente</p> 
+        </a>
+        <a href="mostrarClientes.php" class="card">
+            <p class="xxl">Mostrar los clientes</p> 
+        </a>
+    </div>
 </body>
 </html>
