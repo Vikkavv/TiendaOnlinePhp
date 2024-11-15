3574c77307b9bdc8a2e2a73ccecf14001b37b737
Vikkavv <96725063+Vikkavv@users.noreply.github.com>
2024-11-15 11:58:17 +0100

    Add files via upload

--- Vista/gestionProducto.php
+++ Vista/gestionProducto.php
@@ -3,35 +3,45 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Assets/styles.css">
     <title>Document</title>
 </head>
+<?php
+    require_once "../Modelo/DTOCliente.php";
+    require_once "../Modelo/db.php";
+    app_security();
+?>
 <body>
-    <?php
-        require_once "../Modelo/DAOProducto.php";
-        if(isset($_SESSION['delSuccess'])){
-            print '<p>El producto con id '.$_SESSION['delSuccess'].' fue eliminado correctamente.</p>';
-            unset($_SESSION['delSuccess']);
-        }
-    ?>
-    <h1>PRODUCTOS</h1>
-    <table style="border: 1px solid black;">
-        <?php
-            
-            $DAOProducto = new DAOProducto();
-            $products = $DAOProducto->getAllProducts();
-            foreach ($products as $key => $product) {
-                print '<tr>';
-                    print '<td>'. $product->getId() .'</td>';
-                    print '<td><a href="product.php?id='. $product->getId() .'">'. $product->getNombre() .'</a></td>';
-                    print '<td>'. $product->getDescripcion() .'</td>';
-                    print '<td>'. $product->getPrecio() .'€</td>';
-                    print '<td>'. $product->getClienteId().'</td>';
-                    print '<td><a href="deleteProduct.php?id='. $product->getId() .'">Borrar producto</a></td>';
-                print '</tr>';
-            }
-        ?>
-    </table>
-    <a href="insertProduct.php">Añadir producto</a>
-    <a href="updateProduct.php">Actualizar un producto</a>
+    <nav>
+        <div>
+            <?php
+                $class = "hidden";
+                $nombre = "";
+                $apellido = "";
+                if(isset($_SESSION['cliente'])){
+                    $class = "";
+                    $nombre = $_SESSION['cliente']->getNombre();
+                    $apellido = $_SESSION['cliente']->getApellido();
+                }
+                print '<p class="'.$class.'">Administrador '.$nombre.' '.$apellido.'</p>'
+            ?>
+        </div>
+        <div>
+            <a href="index.php">Página Principal</a>
+            <a href="inicioSesion.php">Iniciar Sesión</a>
+            <a href="registro.php">Registrarse</a>
+            <a href="trastienda.php">Trastienda</a>
+            <a href="carrito.php"><img src="Assets/img/carrito.png" alt="icono carrito de la compra"></a>
+        </div>
+    </nav>
+    <h1 class="center xxl">PRODUCTOS</h1>
+    <div class="container grid-2">
+        <a href="insertProduct.php" class="card">
+            <p class="xxl">Crear Producto</p>
+        </a>
+        <a href="show-DelProducts.php" class="card">
+            <p class="xxl">Mostrar y borrar productos</p>
+        </a>
+    </div>
 </body>
 </html>
